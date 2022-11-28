746e40239e0204c7be94a762a7750c45b817770c
Michael Habermacher <habi.spm@bluewin.ch>
2022-11-28 18:33:23 +0100

    Baudratenumschaltung gefixt - Acknowledgmement 050 implementiert - Sleeptime 0.266s vor Baudratenumschaltung eingebaut - Skiprow Pandasimpot neu 0 - Berechnung Auslesezeit implementiert

--- python/connect.py
+++ python/connect.py
@@ -22,29 +22,53 @@ db = MySQLdb.connect(
 )
 cursor = db.cursor()
 
+# port = serial.Serial(
+#     port='/dev/ttyUSB0',
+#     baudrate=300,
+#     parity=serial.PARITY_EVEN,
+#     stopbits=serial.STOPBITS_ONE,
+#     bytesize=serial.SEVENBITS,
+#     timeout=120
+#     )
+msgEnd = '\x03'
+msgEndByte = msgEnd.encode('utf-8')
+
 while True:
-    print(f'{dt.datetime.now()}Verbindung wird hergestellt ...')
+    print('Verbindung wird hergestellt ...')
+    timeStart = dt.datetime.now()
+    print(timeStart.strftime("%d.%m.%Y %H:%M"))
     port = serial.Serial(
-        port='/dev/ttyUSB0',
-        baudrate=300,
-        parity=serial.PARITY_EVEN,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.SEVENBITS,
-        timeout=120
-        )
+    port='/dev/ttyUSB0',
+    baudrate=300,
+    parity=serial.PARITY_EVEN,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.SEVENBITS,
+    timeout=10
+    )
     print('Ablesung beginnt ...')
-    request = bytes.fromhex('2f3f210d0a')
-    port.write(request)
-    port = serial.Serial(
-        port='/dev/ttyUSB0',
-        baudrate=300,
-        parity=serial.PARITY_EVEN,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.SEVENBITS,
-        timeout=120
-        )
+    # request = bytes.fromhex('2f3f210d0a')
+    request = '/?!\r\n'
+    requestByte = request.encode('utf-8')
+    ack = '\x06050\r\n'
+    ackByte = ack.encode('utf-8')
+    
+    # ackByte = bytes.fromhex(ack)
+    
+    port.write(requestByte)
+    # time.sleep(0.27)
+    answereByte = port.readline()
+    # time.sleep(0.27)
+    print(answereByte.decode('utf-8'))
+    # time.sleep(0.27)
+    port.write(ackByte)
+    time.sleep(0.26)
+    # port.send_break()
+    port.baudrate = 9600
+    # port.send_break()
+    # time.sleep(0.27)
     print('Baudrate auf 9600 umgeschaltet')
-    outputByte = port.read(size=1664)
+    outputByte = port.read_until(expected=msgEndByte)
+    # outputByte = port.read(size=1664)
     print(f'{dt.datetime.now()}Fertig')
     
     outputStr = outputByte.decode('utf-8')
@@ -56,7 +80,7 @@ while True:
     dataStr = dataStr.replace('h','')
     dataStr = dataStr.replace('   ', '')
     dataStr = dataStr.replace('', '')
-    df = pd.read_csv(StringIO(dataStr), sep=' ', skiprows=1, skipfooter=7, header=None, engine='python')
+    df = pd.read_csv(StringIO(dataStr), sep=' ', skiprows=0, skipfooter=7, header=None, engine='python')
     df.rename(columns={0: 'obis', 1: 'value'}, inplace=True)
     df = df.set_index(df['obis'])
     
@@ -69,6 +93,10 @@ while True:
     db.commit()
         
     print(outputStr)
+    timeEnd = dt.datetime.now()
+    readingTime = timeEnd - timeStart
+    print(timeEnd.strftime("%d.%m.%Y %H:%M"))
+    print(f'Auslesedauer: {readingTime}')
     print('Warte 5 Sekunden ...')
     time.sleep(5)
     
