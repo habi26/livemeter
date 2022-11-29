5e8a021ca97314f6299962c12205c264cf8416f4
Michael Habermacher <habi.spm@bluewin.ch>
2022-11-29 12:18:54 +0100

    - Timeout Serial Port angepasst - Error_Bad_Lines=False für Pandas Import

--- python/connect.py
+++ python/connect.py
@@ -43,7 +43,7 @@ while True:
     parity=serial.PARITY_EVEN,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.SEVENBITS,
-    timeout=10
+    timeout=20
     )
     print('Ablesung beginnt ...')
     # request = bytes.fromhex('2f3f210d0a')
@@ -61,18 +61,18 @@ while True:
     print(answereByte.decode('utf-8'))
     # time.sleep(0.27)
     port.write(ackByte)
-    time.sleep(0.26)
+    time.sleep(0.266)
     # port.send_break()
     port.baudrate = 9600
     # port.send_break()
     # time.sleep(0.27)
     print('Baudrate auf 9600 umgeschaltet')
     outputByte = port.read_until(expected=msgEndByte)
-    # outputByte = port.read(size=1664)
+    # outputByte = port.read(size=8000)
     print(f'{dt.datetime.now()}Fertig')
     
     outputStr = outputByte.decode('utf-8')
-    dataStr = outputStr.replace('(', ' ')
+    dataStr = outputStr.replace('(', ';')
     dataStr = dataStr.replace(')', '')
     dataStr = dataStr.replace('*kWh','')
     dataStr = dataStr.replace('*kW','')
@@ -80,7 +80,7 @@ while True:
     dataStr = dataStr.replace('h','')
     dataStr = dataStr.replace('   ', '')
     dataStr = dataStr.replace('', '')
-    df = pd.read_csv(StringIO(dataStr), sep=' ', skiprows=0, skipfooter=7, header=None, engine='python')
+    df = pd.read_csv(StringIO(dataStr), sep=';', skiprows=0, skipfooter=0, header=None, engine='python', error_bad_lines=False)
     df.rename(columns={0: 'obis', 1: 'value'}, inplace=True)
     df = df.set_index(df['obis'])
     
